533d7a87c55ede7baf5d5136b2e347e6e892452c
278deco <278deco.officiel@gmail.com>
2022-11-05 02:38:22 +0100

    Reworked all methods of the ImageHelper

--- src/main/java/apbiot/core/helper/ImageHelper.java
+++ src/main/java/apbiot/core/helper/ImageHelper.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import apbiot.core.MainInitializer;
-import apbiot.core.handler.EImageHandler;
 import apbiot.core.img.ConstructedImage;
 import apbiot.core.img.ConstructedImage.ImageStatus;
 import apbiot.core.objects.Tuple;
@@ -14,17 +13,18 @@ public class ImageHelper {
 	
 	/**
 	 * Convert a discord image into a constructed image
+	 * @param directory - the directory of the image
 	 * @param discImage - the discord image
 	 * @param imageName - the name of the future image
 	 * @see discord4j.rest.util.Image
 	 * @see apbiot.core.img.ConstructedImage
 	 * @return an instance of ConstructedImage
 	 */
-	public static ConstructedImage convertDiscordImage(Image discImage, String imageName) {
-		if(EImageHandler.getTemporaryDirectory() == null || EImageHandler.getPermanentDirectory() == null) throw new NullPointerException("ImageHandler hasn't yet been initialized !");
+	public static ConstructedImage convertDiscordImage(String directory, Image discImage, String imageName) {
+		if(directory == null || directory.isEmpty() || directory.isBlank()) throw new NullPointerException("The directory isn't correctly defined");
 		
 		try {
-			return new ConstructedImage(EImageHandler.getTemporaryDirectory().getAbsolutePath(), imageName, discImage.getData(), ImageStatus.TEMPORARY);
+			return new ConstructedImage(directory, imageName, discImage.getData(), ImageStatus.TEMPORARY);
 		} catch (IOException e) {
 			MainInitializer.LOGGER.warn("Unexpected error while creating a image",e);
 		}
@@ -33,13 +33,14 @@ public class ImageHelper {
 	
 	/**
 	 * Save an image in the temporary folder and format it ready to be sent
+	 * @param directory - the directory of the image
 	 * @param img - the ConstructedImage instance
 	 * @return a tuple containing the image name and the path to the image
 	 */
-	public static Tuple<String, String> getValidDiscordImage(ConstructedImage img) {
-		if(EImageHandler.getTemporaryDirectory() == null || EImageHandler.getPermanentDirectory() == null) throw new NullPointerException("ImageHandler hasn't yet been initialized !");
+	public static Tuple<String, String> getValidDiscordImage(String directory, ConstructedImage img) {
+		if(directory == null || directory.isEmpty() || directory.isBlank()) throw new NullPointerException("The directory isn't correctly defined");
 		
-		String path = EImageHandler.getTemporaryDirectory().getAbsolutePath()+File.separator+img.getName();
+		String path = directory+File.separator+img.getName();
 		
 		try {
 			img.saveImage(new File(path));
@@ -52,13 +53,14 @@ public class ImageHelper {
 	
 	/**
 	 * Delete an file from the temporary folder
-	 * @param imagePath
+	 * @param directory - the directory of the image
+	 * @param imageName - the name of the image and it extension
 	 * @return if the image has been successfully deleted
 	 */
-	public static boolean deleteTemporaryImage(String imageName) {
-		if(EImageHandler.getTemporaryDirectory() == null || EImageHandler.getPermanentDirectory() == null) throw new NullPointerException("ImageHandler hasn't yet been initialized !");
+	public static boolean deleteTemporaryImage(String directory, String imageName) {
+		if(directory == null || directory.isEmpty() || directory.isBlank()) throw new NullPointerException("The directory isn't correctly defined");
 		
-		return new File(EImageHandler.getTemporaryDirectory().getAbsolutePath()+File.separator+imageName).delete();
+		return new File(directory+File.separator+imageName).delete();
 	}
 	
 }
