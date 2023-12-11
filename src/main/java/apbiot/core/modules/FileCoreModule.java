26764e10ebd02ca4f48201e55b12610ab3358a74
278deco <278deco.officiel@gmail.com>
2023-12-11 23:49:23 +0100

    Updated FileCoreModule and DirectoriesLoadedEvent

--- src/main/java/apbiot/core/modules/FileCoreModule.java
+++ src/main/java/apbiot/core/modules/FileCoreModule.java
@@ -1,6 +1,10 @@
 package apbiot.core.modules;
 
+import java.io.IOException;
+import java.util.Set;
 import java.util.UUID;
+
+import javax.management.InstanceNotFoundException;
 
 import apbiot.core.modules.exceptions.CoreModuleLaunchingException;
 import apbiot.core.modules.exceptions.CoreModuleLoadingException;
@@ -8,40 +12,64 @@ import apbiot.core.modules.exceptions.CoreModuleShutdownException;
 import apbiot.core.pems.ProgramEvent;
 import apbiot.core.pems.ProgramEvent.EventPriority;
 import apbiot.core.pems.events.DirectoriesLoadedEvent;
+import marshmalliow.core.builder.DirectoryManager;
+import marshmalliow.core.builder.IOCacheManager;
+import marshmalliow.core.builder.IOFactory;
+import marshmalliow.core.objects.Directory;
 
 public class FileCoreModule extends CoreModule {
 
+	private DirectoryManager directoryManager;
+	
 	protected FileCoreModule() {
 		super(UUID.randomUUID());
 	}
 
 	@Override
 	public void executeAssertion() {
-		
+		try {
+			Class.forName("marshmalliow.core.database.DirectoryManager");
+			Class.forName("marshmalliow.core.database.IOCacheManager");
+		}catch(ClassNotFoundException e) {
+			System.err.println("Cannot find reference for MarshmallIOw class DirectoryManager or IOCacheManager. Aborting launch...");
+			System.exit(-1);
+		}
 	}
 
 	@Override
 	public void init() throws CoreModuleLoadingException {
-		// TODO Auto-generated method stub
+		this.coreHealthy.set(true);
 		
+		this.directoryManager = new DirectoryManager();
+		IOCacheManager.get();
+		IOFactory.get();
+		
+		try {
+			IOFactory.bindDirectoryManager(this.directoryManager);
+		} catch (InstanceNotFoundException e) {
+			throw new CoreModuleLoadingException("", e);
+		}
 	}
 
 	@Override
 	public void launch() throws CoreModuleLaunchingException {
-		// TODO Auto-generated method stub
-		
+		//TODO Load configuration
 	}
 
 	@Override
 	public void shutdown() throws CoreModuleShutdownException {
-		// TODO Auto-generated method stub
-		
+		try {
+			IOCacheManager.get().saveAll();
+		} catch (IOException e) {
+			throw new CoreModuleShutdownException("Couldn't shutdown saved files...", e);
+		}
 	}
 	
 	@Override
 	public void onEventReceived(ProgramEvent e, EventPriority priority) {
 		if(e instanceof DirectoriesLoadedEvent) {
-			//TODO
+			final Set<Directory> directories = ((DirectoriesLoadedEvent)e).getDirectories();
+			if(this.directoryManager != null && directories != null) this.directoryManager.registerNewDirectories(directories);
 		}
 	}
 
--- src/main/java/apbiot/core/pems/events/DirectoriesLoadedEvent.java
+++ src/main/java/apbiot/core/pems/events/DirectoriesLoadedEvent.java
@@ -1,6 +1,9 @@
 package apbiot.core.pems.events;
 
+import java.util.Set;
+
 import apbiot.core.pems.ProgramEvent;
+import marshmalliow.core.objects.Directory;
 
 public class DirectoriesLoadedEvent extends ProgramEvent {
 
@@ -8,6 +11,13 @@ public class DirectoriesLoadedEvent extends ProgramEvent {
 		super(arguments);
 	}
 
+	@SuppressWarnings("unchecked")
+	public Set<Directory> getDirectories() {
+		final Object obj = getEventArgument(0);
+		
+		return obj != null ? (Set<Directory>)obj : Set.of();
+	}
+	
 	@Override
 	public EventPriority getPriority() {
 		return EventPriority.INTERMEDIATE;
