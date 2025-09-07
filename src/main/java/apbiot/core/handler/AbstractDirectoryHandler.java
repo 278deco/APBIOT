706be4ab8e55930506e011c0b6be2eb5666edef6
278deco <278deco.officiel@gmail.com>
2025-09-07 02:02:44 +0200

    chore(AbstractDirectoryHandler): deprecated class

--- src/main/java/apbiot/core/handler/AbstractDirectoryHandler.java
+++ src/main/java/apbiot/core/handler/AbstractDirectoryHandler.java
@@ -9,6 +9,15 @@ import apbiot.core.pems.ProgramEventManager;
 import discord4j.core.GatewayDiscordClient;
 import marshmalliow.core.objects.Directory;
 
+/**
+ * This class is used to handle directories in the system.<br/>
+ * It allows to register directories and dispatch an event when directories are loaded.
+ * @author 278deco
+ * @version 2.0.0
+ * @see apbiot.core.handler.Handler
+ * @deprecated since 6.2.3. Use {@link apbiot.core.pems.events.DirectoryManagerReadyEvent} instead.
+ */
+@Deprecated(since = "6.2.3", forRemoval = true)
 public abstract class AbstractDirectoryHandler extends Handler {
 
 	private final Set<Directory> directories = new HashSet<>();
