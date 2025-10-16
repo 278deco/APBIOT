02bf1d396d5a0516a9e213c903ab6ec53711446c
278deco <278deco.officiel@gmail.com>
2025-10-16 18:24:27 +0200

    chore(AbstractDirectoryHandler): deprecated class

--- src/main/java/apbiot/core/handler/AbstractDirectoryHandler.java
+++ src/main/java/apbiot/core/handler/AbstractDirectoryHandler.java
@@ -1,13 +1,10 @@
 package apbiot.core.handler;
 
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
-import apbiot.core.pems.BaseProgramEventEnum;
-import apbiot.core.pems.ProgramEventManager;
 import discord4j.core.GatewayDiscordClient;
-import marshmalliow.core.objects.Directory;
+import marshmalliow.core.directory.Directory;
 
 /**
  * This class is used to handle directories in the system.<br/>
@@ -15,7 +12,7 @@ import marshmalliow.core.objects.Directory;
  * @author 278deco
  * @version 2.0.0
  * @see apbiot.core.handler.Handler
- * @deprecated since 6.2.3. Use {@link apbiot.core.pems.events.DirectoryManagerReadyEvent} instead.
+ * @deprecated since 6.2.3, for removal in future versions. Use alternative directory management solutions.
  */
 @Deprecated(since = "6.2.3", forRemoval = true)
 public abstract class AbstractDirectoryHandler extends Handler {
@@ -26,17 +23,13 @@ public abstract class AbstractDirectoryHandler extends Handler {
 		this.directories.add(dir);
 	}
 	
-	protected final void addNewDirectory(String name, Path dir) {
-		this.directories.add(new Directory(name, dir));
-	}
-	
 	protected abstract void registerDirectories();
 	
 	@Override
 	protected void register(GatewayDiscordClient client) throws HandlerRegisteringException {
 		registerDirectories();
 		
-		ProgramEventManager.get().dispatchEvent(BaseProgramEventEnum.DIRECTORIES_LOADED_EVENT, new Object[] {directories});
+//		EventBus.get().dispatchEvent(BaseProgramEventEnum.DIRECTORIES_LOADED_EVENT, new Object[] {directories});
 	}
 
 	@Override
